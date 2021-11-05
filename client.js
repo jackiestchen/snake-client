54a9cc4ba4e7549ffd2b9b5d80b97fd368ad5fae
jackiestchen <jackiestchen@gmail.com>
2021-11-04 20:54:55 -0600

    connect function

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+
+module.exports = connect;
+
+
+// console.log("Connecting ...");
+// connect();
