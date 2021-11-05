a50f19f4ed3ddad7ebbd9222dcd3def617bb36bb
jackiestchen <jackiestchen@gmail.com>
2021-11-04 22:06:23 -0600

    Add AWSD controls

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,6 +14,12 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key === "W" || key === "w") {connection.write("Move: up")}
+  if (key === "S" || key === "s") {connection.write("Move: down")}
+  if (key === "A" || key === "a") {connection.write("Move: left")}
+  if (key === "D" || key === "d") {connection.write("Move: right")}
+
 }
 
 module.exports = {
