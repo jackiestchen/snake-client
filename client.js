cfed8843413ef980d9c294bf4e839e4a59c5aba5
jackiestchen <jackiestchen@gmail.com>
2021-11-17 19:22:17 -0700

    Eslint files

--- client.js
+++ client.js
@@ -5,8 +5,7 @@ const connect = () => {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
-    }, 
-    
+  },
   );
 
   conn.setEncoding('utf8');
@@ -14,26 +13,11 @@ const connect = () => {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name: Jac');
-    // setTimeout(() => {conn.write('Move: up')}, 1000);
-    // setTimeout(() => {conn.write('Move: right')}, 2000);
-    // setTimeout(() => {conn.write('Move: up')}, 3000);
-    // setTimeout(() => {conn.write('Move: left')}, 4000);
+
   });
-
-
-
-  // conn.on("data", (data) => {
-  //   console.log(data);
-  //   data.write("Name: Jack")
-  // })
 
   return conn;
 };
 
-
 module.exports = {connect};
 
-
-
-// console.log("Connecting ...");
-// connect();
